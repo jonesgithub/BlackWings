baf862fa1074f4cc610707a14ba74ca085f46ab2
fusijie <fusijie@vip.qq.com>
2014-07-02 02:10:09 +0800

    Add EncryptUserDefault class.

--- Classes/ConfigManager.cpp
+++ Classes/ConfigManager.cpp
@@ -1,6 +1,6 @@
 #include "ConfigManager.h"
 #include "Configuration.h"
-
+#include "EncryptUserDefault.h"
 
 ConfigManager* ConfigManager::m_pInstance = nullptr;
 ConfigManager::CGarbo ConfigManager::m_garbo;
@@ -11,7 +11,7 @@ ConfigManager::ConfigManager()
 
 bool ConfigManager::isHaveSaveFile()
 {
-    if(!UserDefault::getInstance()->getBoolForKey("isHaveSaveFileXml"))
+    if(!EncryptUserDefault::getInstance()->getBoolForKey("isHaveSaveFileXml"))
     {
         initConfig();
         saveConfig();
@@ -27,39 +27,39 @@ void ConfigManager::readConfig()
     if (isHaveSaveFile())
     {
         //system
-        s_playerConfig.musicVolume = UserDefault::getInstance()->getFloatForKey("musicVolume");
-        s_playerConfig.sfxVolume = UserDefault::getInstance()->getFloatForKey("sfxVolume");
-        s_playerConfig.language = UserDefault::getInstance()->getIntegerForKey("language");
+        s_playerConfig.musicVolume = EncryptUserDefault::getInstance()->getFloatForKey("musicVolume");
+        s_playerConfig.sfxVolume = EncryptUserDefault::getInstance()->getFloatForKey("sfxVolume");
+        s_playerConfig.language = EncryptUserDefault::getInstance()->getIntegerForKey("language");
         
         //player
-        s_playerConfig.stone = UserDefault::getInstance()->getIntegerForKey("stone");
-        s_playerConfig.stoneMax = UserDefault::getInstance()->getIntegerForKey("stoneMax");
-        s_playerConfig.gem = UserDefault::getInstance()->getIntegerForKey("gem");
-        s_playerConfig.stonespeedlevel = UserDefault::getInstance()->getIntegerForKey("stonespeedlevel");
-        s_playerConfig.stonecapacitylevel = UserDefault::getInstance()->getIntegerForKey("stonecapacitylevel");
-        s_playerConfig.usedweapon = UserDefault::getInstance()->getIntegerForKey("usedweapon");
-        s_playerConfig.overstage = UserDefault::getInstance()->getIntegerForKey("overstage");
-        s_playerConfig.killbigenemy = UserDefault::getInstance()->getIntegerForKey("killbigenemy");
-        s_playerConfig.firstkill = UserDefault::getInstance()->getBoolForKey("firstkill");
-        s_playerConfig.unlockallfighter = UserDefault::getInstance()->getBoolForKey("unlockallfighter");
+        s_playerConfig.stone = EncryptUserDefault::getInstance()->getIntegerForKey("stone");
+        s_playerConfig.stoneMax = EncryptUserDefault::getInstance()->getIntegerForKey("stoneMax");
+        s_playerConfig.gem = EncryptUserDefault::getInstance()->getIntegerForKey("gem");
+        s_playerConfig.stonespeedlevel = EncryptUserDefault::getInstance()->getIntegerForKey("stonespeedlevel");
+        s_playerConfig.stonecapacitylevel = EncryptUserDefault::getInstance()->getIntegerForKey("stonecapacitylevel");
+        s_playerConfig.usedweapon = EncryptUserDefault::getInstance()->getIntegerForKey("usedweapon");
+        s_playerConfig.overstage = EncryptUserDefault::getInstance()->getIntegerForKey("overstage");
+        s_playerConfig.killbigenemy = EncryptUserDefault::getInstance()->getIntegerForKey("killbigenemy");
+        s_playerConfig.firstkill = EncryptUserDefault::getInstance()->getBoolForKey("firstkill");
+        s_playerConfig.unlockallfighter = EncryptUserDefault::getInstance()->getBoolForKey("unlockallfighter");
         
         //fighter
         for (int i = 0; i < FIGHTER_MAX; ++i) {
             char name[30];
             sprintf(name,"fighterslocked_%d",i);
-            s_playerConfig.fighterslocked[i] = UserDefault::getInstance()->getBoolForKey(name);
+            s_playerConfig.fighterslocked[i] = EncryptUserDefault::getInstance()->getBoolForKey(name);
             sprintf(name,"fighterslevel_%d",i);
-            s_playerConfig.fighterslevel[i] = UserDefault::getInstance()->getIntegerForKey(name);
+            s_playerConfig.fighterslevel[i] = EncryptUserDefault::getInstance()->getIntegerForKey(name);
         }
         
         //weapon
-        s_playerConfig.weaponlocked = UserDefault::getInstance()->getBoolForKey("weaponlocked");
+        s_playerConfig.weaponlocked = EncryptUserDefault::getInstance()->getBoolForKey("weaponlocked");
         for(int i = 0; i < WEAPON_MAX; ++i){
             char name[30];
             sprintf(name, "weaponslevel_%d",i);
-            s_playerConfig.weaponslevel[i] = UserDefault::getInstance()->getIntegerForKey(name);
+            s_playerConfig.weaponslevel[i] = EncryptUserDefault::getInstance()->getIntegerForKey(name);
             sprintf(name, "weaponCount_%d", i);
-            s_playerConfig.weaponCount[i] = UserDefault::getInstance()->getIntegerForKey(name);
+            s_playerConfig.weaponCount[i] = EncryptUserDefault::getInstance()->getIntegerForKey(name);
         }
         
         //medal
@@ -67,9 +67,9 @@ void ConfigManager::readConfig()
         {
             char name[30];
             sprintf(name, "medallocked_%d",i);
-            s_playerConfig.medallocked[i] = UserDefault::getInstance()->getBoolForKey(name);
+            s_playerConfig.medallocked[i] = EncryptUserDefault::getInstance()->getBoolForKey(name);
             sprintf(name, "medalget_%d",i);
-            s_playerConfig.medalget[i] = UserDefault::getInstance()->getBoolForKey(name);
+            s_playerConfig.medalget[i] = EncryptUserDefault::getInstance()->getBoolForKey(name);
         }
     }
 }
@@ -119,45 +119,45 @@ void ConfigManager::initConfig()
         s_playerConfig.medalget[i] = false;
     }
     
-    UserDefault::getInstance()->setBoolForKey("isHaveSaveFileXml", true);
+    EncryptUserDefault::getInstance()->setBoolForKey("isHaveSaveFileXml", true);
 }
 
 void ConfigManager::saveConfig()
 {
     //system
-    UserDefault::getInstance()->setFloatForKey("musicVolume", s_playerConfig.musicVolume);
-    UserDefault::getInstance()->setFloatForKey("sfxVolume", s_playerConfig.sfxVolume);
-    UserDefault::getInstance()->setIntegerForKey("language", (int)s_playerConfig.language);
+    EncryptUserDefault::getInstance()->setFloatForKey("musicVolume", s_playerConfig.musicVolume);
+    EncryptUserDefault::getInstance()->setFloatForKey("sfxVolume", s_playerConfig.sfxVolume);
+    EncryptUserDefault::getInstance()->setIntegerForKey("language", (int)s_playerConfig.language);
     
     //player
-    UserDefault::getInstance()->setIntegerForKey("stone", s_playerConfig.stone);
-    UserDefault::getInstance()->setIntegerForKey("stoneMax", s_playerConfig.stoneMax);
-    UserDefault::getInstance()->setIntegerForKey("gem", s_playerConfig.gem);
-    UserDefault::getInstance()->setIntegerForKey("stonespeedlevel", s_playerConfig.stonespeedlevel);
-    UserDefault::getInstance()->setIntegerForKey("stonecapacitylevel", s_playerConfig.stonecapacitylevel);
-    UserDefault::getInstance()->setIntegerForKey("usedweapon", s_playerConfig.usedweapon);
-    UserDefault::getInstance()->setIntegerForKey("overstage", s_playerConfig.overstage);
-    UserDefault::getInstance()->setIntegerForKey("killbigenemy", s_playerConfig.killbigenemy);
-    UserDefault::getInstance()->setBoolForKey("firstkill", s_playerConfig.firstkill);
-    UserDefault::getInstance()->setBoolForKey("unlockallfighter", s_playerConfig.unlockallfighter);
+    EncryptUserDefault::getInstance()->setIntegerForKey("stone", s_playerConfig.stone);
+    EncryptUserDefault::getInstance()->setIntegerForKey("stoneMax", s_playerConfig.stoneMax);
+    EncryptUserDefault::getInstance()->setIntegerForKey("gem", s_playerConfig.gem);
+    EncryptUserDefault::getInstance()->setIntegerForKey("stonespeedlevel", s_playerConfig.stonespeedlevel);
+    EncryptUserDefault::getInstance()->setIntegerForKey("stonecapacitylevel", s_playerConfig.stonecapacitylevel);
+    EncryptUserDefault::getInstance()->setIntegerForKey("usedweapon", s_playerConfig.usedweapon);
+    EncryptUserDefault::getInstance()->setIntegerForKey("overstage", s_playerConfig.overstage);
+    EncryptUserDefault::getInstance()->setIntegerForKey("killbigenemy", s_playerConfig.killbigenemy);
+    EncryptUserDefault::getInstance()->setBoolForKey("firstkill", s_playerConfig.firstkill);
+    EncryptUserDefault::getInstance()->setBoolForKey("unlockallfighter", s_playerConfig.unlockallfighter);
     
     //fighter
     for (int i = 0; i < FIGHTER_MAX; ++i) {
         char name[30];
         sprintf(name,"fighterslocked_%d",i);
-        UserDefault::getInstance()->setBoolForKey(name, s_playerConfig.fighterslocked[i]);
+        EncryptUserDefault::getInstance()->setBoolForKey(name, s_playerConfig.fighterslocked[i]);
         sprintf(name,"fighterslevel_%d",i);
-        UserDefault::getInstance()->setIntegerForKey(name, s_playerConfig.fighterslevel[i]);
+        EncryptUserDefault::getInstance()->setIntegerForKey(name, s_playerConfig.fighterslevel[i]);
     }
     
     //weapon
-    UserDefault::getInstance()->setBoolForKey("weaponlocked",s_playerConfig.weaponlocked);
+    EncryptUserDefault::getInstance()->setBoolForKey("weaponlocked",s_playerConfig.weaponlocked);
     for(int i = 0; i < WEAPON_MAX; ++i){
         char name[30];
         sprintf(name, "weaponslevel_%d",i);
-        UserDefault::getInstance()->setIntegerForKey(name, s_playerConfig.weaponslevel[i]);
+        EncryptUserDefault::getInstance()->setIntegerForKey(name, s_playerConfig.weaponslevel[i]);
         sprintf(name, "weaponCount_%d", i);
-        UserDefault::getInstance()->setIntegerForKey(name, s_playerConfig.weaponCount[i]);
+        EncryptUserDefault::getInstance()->setIntegerForKey(name, s_playerConfig.weaponCount[i]);
     }
     
     //medal
@@ -165,9 +165,9 @@ void ConfigManager::saveConfig()
     {
         char name[30];
         sprintf(name, "medallocked_%d",i);
-        UserDefault::getInstance()->setBoolForKey(name, s_playerConfig.medallocked[i]);
+        EncryptUserDefault::getInstance()->setBoolForKey(name, s_playerConfig.medallocked[i]);
         sprintf(name, "medalget_%d",i);
-        UserDefault::getInstance()->setBoolForKey(name, s_playerConfig.medalget[i]);
+        EncryptUserDefault::getInstance()->setBoolForKey(name, s_playerConfig.medalget[i]);
     }
     
     UserDefault::getInstance()->flush();
@@ -176,20 +176,20 @@ void ConfigManager::saveConfig()
 void ConfigManager::saveMusicVolume(float volume)
 {
     s_playerConfig.musicVolume = volume;
-    UserDefault::getInstance()->setFloatForKey("musicVolume", volume);
+    EncryptUserDefault::getInstance()->setFloatForKey("musicVolume", volume);
     UserDefault::getInstance()->flush();
 }
 
 void ConfigManager::saveSFXVolume(float volume)
 {
     s_playerConfig.sfxVolume = volume;
-    UserDefault::getInstance()->setFloatForKey("sfxVolume", volume);
+    EncryptUserDefault::getInstance()->setFloatForKey("sfxVolume", volume);
     UserDefault::getInstance()->flush();
 }
 
 void ConfigManager::saveLanguage(int language)
 {
     s_playerConfig.language = language;
-    UserDefault::getInstance()->setFloatForKey("language", language);
+    EncryptUserDefault::getInstance()->setFloatForKey("language", language);
     UserDefault::getInstance()->flush();
 }
